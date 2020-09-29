91ed12db7fb211c9aaf0879d37ee89e42266a856
Pratik <priyadarshanpratik123@gmail.com>
2020-09-29 11:49:44 +0530

    SVM Exercises Done

--- machine-learning-ex/ex6/gaussianKernel.m
+++ machine-learning-ex/ex6/gaussianKernel.m
@@ -9,14 +9,9 @@ x1 = x1(:); x2 = x2(:);
 % You need to return the following variables correctly.
 sim = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
-
-
+numerator = sum((x1-x2).^2);
+denominator = 2*sigma*sigma;
+sim = exp(-numerator/denominator);
 
 
 
